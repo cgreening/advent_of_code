e0f85f7ab429c0743e0a7b0487d440a6fce165b9
Chris Greening <chris@cmgresearch.com>
2020-12-05 14:36:17 +0000

    fix seats

--- day5/seats.ts
+++ day5/seats.ts
@@ -6,33 +6,8 @@ FFFBBBFRRR: row 14, column 7, seat ID 119.
 BBFFBBFRLL: row 102, column 4, seat ID 820.
 */
 
-function binarySearch(bd: string, up: string, max: number) {
-  let min = 0;
-  // first 7 characters
-  const rowChars = bd.split("");
-  for (const c of rowChars) {
-    const half = Math.floor((min + max) / 2);
-    if (c === up) {
-      min = half;
-    } else {
-      max = half;
-    }
-  }
-  return max;
-}
-
-function getRow(bd: string) {
-  return binarySearch(bd.slice(0, 7), "B", 127);
-}
-
-function getColumn(bd: string) {
-  return binarySearch(bd.slice(7, 10), "R", 7);
-}
-
 function getSeat(bd: string) {
-  const row = getRow(bd);
-  const col = getColumn(bd);
-  return row * 8 + col;
+  return parseInt(bd.replace(/[B|R]/g, "1").replace(/[F|L]/g, "0"), 2);
 }
 
 const seats = numbers.map(getSeat);
