eeccb85572c275000e24c65302fd465f8cbcd9de
Chris Greening <chris@cmgresearch.com>
2020-12-06 17:49:56 +0000

    clean up

--- day6/questions.ts
+++ day6/questions.ts
@@ -15,28 +15,29 @@ type GroupAnswers = {
 
 const input = fs.readFileSync(__dirname + "/input.txt", { encoding: "utf-8" });
 
-const answers: GroupAnswers[] = input.split("\n\n").map((data) => {
-  const invidualsAnserts = data
-    .split("\n")
-    .map((row) => new Set(row.split("")));
+const groupsAnswers: GroupAnswers[] = input.split("\n\n").map((data) => {
+  const groupAnswers = data.split("\n").map((row) => new Set(row.split("")));
   return {
-    union: invidualsAnserts.reduce(
-      (u, invidual) => union(u, invidual),
+    union: groupAnswers.reduce(
+      (previous, current) => union(previous, current),
       new Set<string>()
     ),
-    intersection: invidualsAnserts.reduce(
-      (u, invidual) => (u ? intersection(u, invidual) : invidual),
-      null as Set<string> | null // initial value is null
+    intersection: groupAnswers.reduce(
+      // for the first row just use the set, otherwise intersect with the previous
+      (previous, current) =>
+        previous ? intersection(previous, current) : current,
+      null as Set<string> | null
     ),
   };
 });
 
-const sum1 = answers
+const sum1 = groupsAnswers
   .map((ans) => ans.union.size)
   .reduce((c, total) => total + c, 0);
+
 console.log(sum1);
 
-const sum2 = answers
+const sum2 = groupsAnswers
   .map((ans) => (ans.intersection || new Set()).size)
   .reduce((c, total) => total + c, 0);
 
