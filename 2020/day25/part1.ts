3dd79ef2e8f9aab68ce8e8a54b523ff3559e16b6
Chris Greening <chris@cmgresearch.com>
2020-12-25 08:08:09 +0000

    Day21

--- 2020/day25/part1.ts
+++ 2020/day25/part1.ts
@@ -1,7 +1,7 @@
 import { isConstructorDeclaration } from "typescript";
 import numbers from "../day5/seat_numbers";
 
-function day25Part1(cardNumbr: bigint, doornumber: bigint) {
+function day25Part1(n1: bigint, n2: bigint) {
   const divisor = 20201227n;
   const transform = (subjectNumber: bigint, loops: number) => {
     let value = 1n;
@@ -14,22 +14,23 @@ function day25Part1(cardNumbr: bigint, doornumber: bigint) {
 
   const calcLoops = (finalNumber: bigint) => {
     let value = 1n;
-    for (let i = 0; i < 10000000; i++) {
+    let loops = 1;
+    while (true) {
       value = value * 7n;
       value = value % divisor;
       if (value === finalNumber) {
-        return i + 1;
+        return loops;
       }
+      loops++;
     }
-    throw new Error("Cannot work out loops");
   };
 
-  const cardLoops = calcLoops(cardNumbr);
-  const doorLoops = calcLoops(doornumber);
-
-  const result = transform(cardNumbr, doorLoops);
+  console.time("day25");
+  const doorLoops = calcLoops(n2);
+  const result = transform(n1, doorLoops);
+  console.timeEnd("day25");
   console.log("Part1", result);
 }
 
-// day25Part1(5764801n, 17807724n);
+// example day25Part1(5764801n, 17807724n);
 day25Part1(1965712n, 19072108n);
